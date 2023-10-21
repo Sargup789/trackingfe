a9818d6696dcefc573e71b7adc97b14efa6df400
Sarthak Gupta <sarthak.gupta@bolt.earth>
2023-10-21 16:08:57 +0530

    fixes

--- components/TaskComponents/AddTaskDialog.tsx
+++ components/TaskComponents/AddTaskDialog.tsx
@@ -77,7 +77,7 @@ const AddTaskDialog: React.FC<TaskDialogProps> = ({
     data: trucks,
     isLoading,
     refetch,
-  }: UseQueryResult<TruckData[], unknown> = useQuery(["trucks", page, size, filtersState], () => fetchTrucks(page, size), {
+  }: UseQueryResult<TruckData[], unknown> = useQuery(["trucks", page, size], () => fetchTrucks(page, size), {
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
   });
